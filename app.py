f2ab1473a2d77b7fddf237b942400c89eb171bd7
karishmamuskan4 <karishmamuskan248@gmail.com>
2026-05-21 18:22:44 +0530

    model changed

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def analyze_gap():
         """
         
         completion = client.chat.completions.create(
-            model="llama3-8b-8192", 
+            model="mixtral-8x7b-32768", 
             messages=[{"role": "user", "content": prompt}],
             temperature=0.1,
             max_tokens=300
